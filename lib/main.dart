4ccaecf319669ea89c81ce8eb4fc9c6c8b91cd9a
mrjuoss <mrjuoss@gmail.com>
2019-04-11 10:09:02 +0700

    Add EditText, Raised Button, Flat Button, Icon Button, Check Box, Slider and Date Picker

--- lib/main.dart
+++ lib/main.dart
@@ -13,42 +13,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _value = 'New Value from Variable';
-  String _dateNow = '';
 
-  String _message = '';
-
-  int _jumlah = 0;
-
+  String _value = 'Default Value from Variable';
+  /* Tanpa Parameter */
   void _onPressed() {
     setState(() {
       _value = 'My Name is Jaki';
     });
   }
 
+  /* Parameter */
   void _onPressedAgain(String value) {
     setState(() {
       _value = value;
     });
   }
 
-  void _onPressedFlatButton() {
-    setState(() {
-      _dateNow = DateTime.now().toString();
-    });
-  }
-
-  void _add() {
-    setState(() {
-      _jumlah++;
-    });
-  }
-
-  void _substract() {
-    setState(() {
-      _jumlah--;
-    });
-  }
+  /* Edit Text */
+  String _message = '';
 
   void _onChange(String value) {
     setState(() => _message = 'Changed : $value');
@@ -58,45 +40,136 @@ class _MyAppState extends State<MyApp> {
     setState(() => _message = 'Submited : $value');
   }
 
+  /* Flat Button */
+  String _dateNow = '';
+
+  void _onPressedFlatButton() {
+    setState(() {
+      _dateNow = DateTime.now().toString();
+    });
+  }
+
+  /* Icon Button */
+  int _jml = 0;
+
+  void _add() {
+    setState(() {
+      _jml++;
+    });
+  }
+
+  void _substract() {
+    setState(() {
+      _jml--;
+    });
+  }
+
+  /* Checkbox */
+  bool _pilih1 = false;
+  bool _pilih2 = false;
+
+  void _pilih1Change(bool value) => setState(() => _pilih1 = value);
+
+  void _pilih2Change(bool value) => setState(() => _pilih2 = value);
+
+  /* Slider */
+  double _sliderValue = 0.0;
+
+  void _setSliderValue(double value) => setState(() => _sliderValue = value);
+
+  /* Date Picker */
+  String _dateValue = '';
+
+  Future _selectDate() async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2020)
+    );
+
+    if (picked != null) setState(() => _dateValue = picked.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: new AppBar(
-        title: new Text(_value),
+        title: new Text('Flutter BAsic App'),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
             new Text(_value),
-            new Text(_dateNow),
-            new Text('Nilainya adalah $_jumlah'),
-            new Text(_message),
+
+            /* Tanpa Parameter */
             new RaisedButton(
               onPressed: _onPressed,
               child: new Text('Click Me'),
             ),
+
+            /* Parameter */
             new RaisedButton(
               onPressed: () => _onPressedAgain('Hello Bryan'),
               child: new Text('Click Me Again'),
             ),
+
+            /* Flat Button */
             new FlatButton(
                 onPressed: _onPressedFlatButton,
                 child: new Text('Click Flat Button')),
+            new Text(_dateNow),
+
+            /* Icon Button */
             new IconButton(icon: Icon(Icons.add), onPressed: _add),
+
+            new Text('Nilainya adalah $_jml'),
+
             new IconButton(icon: Icon(Icons.remove), onPressed: _substract),
+
+            /* Edit Text */
+            new Text(_message),
             new TextField(
               decoration: new InputDecoration(
-                labelText: 'Hello',
-                hintText: 'Input your message',
-                icon: new Icon(Icons.people)
-              ),
+                  labelText: 'Hello',
+                  hintText: 'Input your message',
+                  icon: new Icon(Icons.people)),
               autofocus: true,
-              keyboardType: TextInputType.number,
+              keyboardType: TextInputType.text,
               onChanged: _onChange,
               onSubmitted: _onSubmit,
             ),
+
+            /* Checbox*/
+            new Checkbox(
+                value: _pilih1,
+                onChanged: _pilih1Change,
+                activeColor: Colors.amberAccent),
+            new CheckboxListTile(
+              value: _pilih2,
+              onChanged: _pilih2Change,
+              title: new Text('CheckboxListTile'),
+              subtitle: new Text('Sub Title'),
+              secondary: new Icon(Icons.archive),
+              activeColor: Colors.red,
+              controlAffinity: ListTileControlAffinity.trailing,
+            ),
+
+            /* Slider */
+            new Text('Value Slider : ${(_sliderValue * 100).round()}'),
+            new Slider(value: _sliderValue, onChanged: _setSliderValue),
+
+            /* Date Picker */
+            new Text(_dateValue),
+            new RaisedButton(
+                onPressed: _selectDate,
+                child: new Text('Select Date!'),
+            ),
+
+
+
           ],
         ),
       ),
